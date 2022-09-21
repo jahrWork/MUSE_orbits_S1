7681441ce66186ef1d3f27f7d3972079d8f769d2
Juan Antonio Hernandez Ramos <jahramos@gmail.com>
2022-09-21 14:07:24 +0200

    new versions

--- sources/Milestone1.py
+++ sources/Milestone1.py
@@ -7,27 +7,132 @@ not to see data flow and to have reusable and easy to maintein codes.
 The idea is to mimic mathmatical concepts or abstractions.  
 """
 
-from numpy import array, zeros
+from numpy import array, zeros, linspace
 import matplotlib.pyplot as plt 
 
 
-U = array( [ 1, 0, 0, 1 ])
+def first_version(): 
 
-N = 200 
-x = array( zeros(N) )
-y = array( zeros(N) )
-x[0] = U[0] 
-y[0] = U[1]
+    U = array( [ 1, 0, 0, 1 ])
+    
+    N = 200 
+    x = array( zeros(N) )
+    y = array( zeros(N) )
+    x[0] = U[0] 
+    y[0] = U[1]
+    
+    for i in range(1, N): 
+    
+      F = array( [ U[2], U[3], -U[0]/(U[0]**2+U[1]**2)**1.5, -U[1]/(U[0]**2+U[1]**2)**1.5 ] ) 
+      dt = 0.1 
+      U = U + dt * F 
+      x[i] = U[0] 
+      y[i] = U[1]
+    
+    plt.plot(x, y)
+    plt.show()
 
-for i in range(1, N): 
+def abstraction_for_F(): 
 
-  F = array( [ U[2], U[3], -U[0]/(U[0]**2+U[1]**2)**1.5, -U[1]/(U[0]**2+U[1]**2)**1.5 ] ) 
-  dt = 0.1 
-  U = U + dt * F 
-  x[i] = U[0] 
-  y[i] = U[1]
+    U = array( [ 1, 0, 0, 1 ])
+    
+    N = 200 
+    x = array( zeros(N) )
+    y = array( zeros(N) )
+    t = array( zeros(N) )
+    x[0] = U[0] 
+    y[0] = U[1]
+    t[0] = 0 
+    
+    for i in range(1, N): 
 
-plt.plot(x, y)
-plt.show()
+      dt = 0.1 
+      t[i] = dt*i
+      F = Kepler( U, t[i-1])
+    
+      U = U + dt * F 
+      x[i] = U[0] 
+      y[i] = U[1]
+    
+    plt.plot(x, y)
+    plt.show()
 
+
+
+
+def abstraction_for_F_and_Euler(): 
+
+    U = array( [ 1, 0, 0, 1 ])
+    
+    N = 200 
+    x = array( zeros(N) )
+    y = array( zeros(N) )
+    t = array( zeros(N) )
+    x[0] = U[0] 
+    y[0] = U[1]
+    t[0] = 0 
+    
+    for i in range(1, N): 
+
+      dt = 0.1 
+      t[i] = dt*i
+      U = Euler(U, dt, t, Kepler)
+      x[i] = U[0] 
+      y[i] = U[1]
+    
+    plt.plot(x, y)
+    plt.show()
+
+
+"""
+This is our final version to integrate any problem with any temporal scheme.
+
+Diffetent abstraction : 
+                     1) dU/dt = F(U, t) 
+                     2) Temporal scheme to one step 
+                     3) Cauchy problem to perform different steps 
+
+
+
+"""
+
+
+def abstraction_for_F_and_Euler_and_Cauchy_Problem(): 
+
+    N = 200 
+    U0 = array( [ 1, 0, 0, 1 ])
+    t = linspace(0, 20, N)
+        
+    U =  Cauchy_problem( Kepler, t, U0, Euler) 
+
+    plt.plot(U[:,0] , U[:,1])
+    plt.show()
+
+
+
+
+
+def Kepler(U, t): 
+
+    x = U[0]; y = U[1]; dxdt = U[2]; dydt = U[3]
+    d = ( x**2  +y**2 )**1.5
+
+    return  array( [ dxdt, dydt, -x/d, -y/d ] ) 
+
+def Euler(U, dt, t, F): 
+
+    return U + dt * F(U, t)
+
+def Cauchy_problem( F, t, U0, Temporal_scheme): 
+
+     N, Nv=  len(t)-1, len(U0)
+     U = array( zeros([N+1, Nv] ) )
+
+     U[0,:] = U0
+
+     for n in range(N):
+
+        U[n+1,:] = Temporal_scheme( U[n, :], t[n+1] - t[n], t[n],  F ) 
+
+     return U
 
--- sources/Milestones.py
+++ sources/Milestones.py
@@ -2,5 +2,7 @@
 
 import Milestone1
 
-
-
+Milestone1.first_version()
+Milestone1.abstraction_for_F()
+Milestone1.abstraction_for_F_and_Euler()
+Milestone1.abstraction_for_F_and_Euler_and_Cauchy_Problem()
