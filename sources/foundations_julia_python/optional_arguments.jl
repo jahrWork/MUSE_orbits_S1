a60fc14dfa68e6d492a34393c5134e90598cbc3e
Juan Hernandez <jahramos@gmail.com>
2024-09-24 00:53:52 +0200

    optional

--- sources/foundations_julia_python/optional_arguments.jl
+++ sources/foundations_julia_python/optional_arguments.jl
@@ -10,5 +10,36 @@ function f(x, y=nothing)
          
 end 
 
+function g(; x, y=nothing)
+
+    if !isnothing(y) 
+
+         return x+y
+
+    else 
+         return x 
+    end
+         
+end 
+
+function h(; x::Float64, y=nothing)
+
+    if !isnothing(y) 
+
+         return x+y
+
+    else 
+         return x 
+    end
+         
+end 
+
+
 println("f(x,y) = ", f(2,2))
 println("f(x,y) = ", f(2))
+
+println("g(x,y) = ", g(x=2))
+println("g(x,y) = ", g(x=2, y=3))
+
+println("h(x,y) = ", h( x = 2.0 ) )
+println("h(x,y) = ", h( x = 2.0, y = 3 ) )
