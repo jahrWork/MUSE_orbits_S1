46e751c5097cb73e9b6019a14197cf4597439ffa
Juan Hernandez <jahramos@gmail.com>
2024-10-09 20:26:52 +0200

    plot

--- src/foundations_julia/plot.jl
+++ src/foundations_julia/plot.jl
@@ -5,17 +5,23 @@
 
 using Plots
 
-println("Hello world")
-
 function plot_example()
 
-  t =  Vector( range(0, 1, 100) )
+  N = 100
+  t =  Vector( range(0, 1, N) )
+  y = zeros(N, 3)
 
   for i in 1:3
-    
-    y = sin.( 2  * i * pi * t )
-    display( plot!(t, y) )
-  end 
+        y[:,i] = sin.( 2  * i * pi * t )
+  end       
+  #  display( plot!(t, y) )
+  plot(t, y, 
+       title = "Trigonometric f of different frequency \$ y(t) = \\sin(2  \\pi  f  t) \$", 
+       xlabel = "\$ t \$", ylabel = "\$ y \$", 
+       label = ["f=1" "f=2" "f=3"], lw = 3,
+       xlimits=(0,1), ylimits=(-2,2)
+      )
+  
 
 end 
 
